391d120606e0ab3354311d0fe0590e0c0e6b30b8
Ti64CLi <32717455+Ti64CLi@users.noreply.github.com>
2022-02-18 11:34:24 +0100

    First test

--- graphics.c
+++ graphics.c
@@ -14,7 +14,7 @@ void gFillScreen(buffer_t *screen_buffer, uint16_t color)
     }
 }
 
-void gDrawSprite(buffer_t *screen_buffer, uint16_t x, uint16_t y, sprite_t sprite, uint8_t zoom)
+void gDrawSprite(buffer_t *screen_buffer, int16_t x, int16_t y, sprite_t sprite, uint8_t zoom)
 {
     for (uint16_t i = 0; i < sprite.height; i++)
     {
@@ -25,7 +25,7 @@ void gDrawSprite(buffer_t *screen_buffer, uint16_t x, uint16_t y, sprite_t sprit
                 for (uint8_t jz = 0; jz < zoom; jz++)
                 {
                     uint16_t ycoord = i * zoom + y + iz, xcoord = j * zoom + x + jz;
-                    if (xcoord < screen_buffer->width && ycoord < screen_buffer->height)
+                    if (xcoord > 0 && xcoord < screen_buffer->width && ycoord > 0 && ycoord < screen_buffer->height)
                         screen_buffer->buffer[ycoord * screen_buffer->width + xcoord] = sprite.data[i * sprite.width + j];
                 }
             }
@@ -33,11 +33,11 @@ void gDrawSprite(buffer_t *screen_buffer, uint16_t x, uint16_t y, sprite_t sprit
     }
 }
 
-void gDrawTileMap(buffer_t *screen_buffer, tilemap_t tilemap, uint16_t x, uint16_t y, uint16_t tilemap_x, uint16_t tilemap_y, uint16_t tilemap_w, uint16_t tilemap_h, uint8_t zoom, int8_t xoffset, int8_t yoffset)
+void gDrawTileMap(buffer_t *screen_buffer, tilemap_t tilemap, int16_t x, int16_t y, uint16_t tilemap_x, uint16_t tilemap_y, uint16_t tilemap_w, uint16_t tilemap_h, uint8_t zoom, int8_t xoffset, int8_t yoffset)
 {
-    for (uint16_t i = 0; i < tilemap_h; i++)
+    for (uint16_t i = 0; i <= tilemap_h; i++)
     {
-        for (uint16_t j = 0; j < tilemap_w; j++)
+        for (uint16_t j = 0; j <= tilemap_w; j++)
         {
             gDrawSprite(screen_buffer, x + j * tilemap.tile_width * zoom + xoffset, y + i * tilemap.tile_height * zoom + yoffset, tilemap.tiles[tilemap.tilemap[(i + tilemap_y) * tilemap.width + j + tilemap_x]], zoom);
         }
